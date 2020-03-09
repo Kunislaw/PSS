cd7f566cef9dd96cee5136ef1e1a22b179f97a85
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2020-03-09 20:59:27 +0100

    Add Id to models

--- src/main/java/com/pss/pssapp/Models/User.java
+++ src/main/java/com/pss/pssapp/Models/User.java
@@ -1,14 +1,22 @@
-package com.pss.pssapp.Models;
+package com.pss.pssapp.models;
 
 import com.sun.istack.NotNull;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import java.util.Date;
 
 @Data
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
     @NotNull
     private String companyName;
     @NotNull
@@ -26,6 +34,5 @@ public class User {
     private boolean status = true;
     private Date registrationDate = new Date();
     private String role = "ROLE_USER";
-
 
 }
