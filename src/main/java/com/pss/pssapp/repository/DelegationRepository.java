351d0449cd292cd15efa2addf3dc5d62660a5bbf
Kunislaw <weyna.karol@gmail.com>
2020-03-20 11:02:10 +0100

    enable swagger

--- src/main/java/com/pss/pssapp/repository/DelegationRepository.java
+++ src/main/java/com/pss/pssapp/repository/DelegationRepository.java
@@ -5,8 +5,12 @@ import com.pss.pssapp.models.Delegation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * DelegationRepository
  */
 @Repository
-public interface DelegationRepository extends JpaRepository<Delegation, Long> {}
+public interface DelegationRepository extends JpaRepository<Delegation, Long> {
+
+}
